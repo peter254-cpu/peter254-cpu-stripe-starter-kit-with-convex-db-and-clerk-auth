1dd883f53cbd923c26c695a4ff353cdcfc8f65c0
kiruhi <peterslap67@gmail.com>
2025-04-04 12:10:24 +0300

    tried to complete queries logic to finish async behaviour

--- convex/courses.ts
+++ convex/courses.ts
@@ -4,14 +4,26 @@ import { query } from "./_generated/server";
 export const getCourses = query({
 	args: {},
 	handler: async (ctx) => {
-		const courses = await ctx.db.query("courses").collect();
-		return courses;
+		try {
+			const courses = await ctx.db.query("courses").collect();
+			return courses;
+		} catch (error) {
+			console.log("Error getting courses", error)
+			throw new Error("Error fetching courses")
+		}
+		
 	},
 });
 
 export const getCourseById = query({
 	args: { courseId: v.id("courses") },
 	handler: async (ctx, args) => {
-		return await ctx.db.get(args.courseId);
-	},
-});
+		try {
+			const course = ctx.db.get(args.courseId);
+			return course
+		} catch (error) {
+			console.log("Error fetching the course", error)
+			throw new Error("Error getting the course")
+		}	
+	}
+})
